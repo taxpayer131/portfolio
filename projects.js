311851b4473674250e969e4116fe125d7ab42bc8
taxpayer131 <chris.stefan1844@gmail.com>
2017-07-13 00:03:55 -0700

    initial commit

---
+++ projects.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var projects = [];
+
+function Projects (rawDataObj) {
+  this.name = rawDataObj.name;
+  this.name = rawData.name;
+  this.tools = rawData.tools;//tools used
+  this.url = rawData.url;
+  this.gitHub = rawData.gitHub;
+  this.about = rawData.about;
+}
+Projects.prototype.toHtml = function() {
+  var $newProject = $('project.template').clone;
+};
+
+
+rawData.forEach(function(projectObject) {
+  // REVIEW: Take a look at this forEach method; This may be the first time we've seen it.
+  project.push(new Project(projectObject));
+});
+
+projects.forEach(function(project) {
+  $('.project').append(project.toHtml());
+});
+}
---
+++ rawdata.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var rawData = [{
+  this.name = rawData.name;
+  this.tools = rawData.tools;//tools used
+  this.url = rawData.url;
+  this.gitHub = rawData.gitHub;
+  this.about = rawData.about;
+}
+];
