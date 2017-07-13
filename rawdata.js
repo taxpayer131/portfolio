e5522105d6ab64feb549714666cda697ae06d08c
taxpayer131 <chris.stefan1844@gmail.com>
2017-07-12 22:39:17 -0700

    commit to save work

---
+++ rawdata.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var rawData = [{
+  this.name = rawData.name;
+  this.tools = rawData.tools;//tools used
+  this.url = rawData.url;
+  this.gitHub = rawData.gitHub;
+  this.about = rawData.about;
+}
+];
+
+Projects.prototype.toHtml
